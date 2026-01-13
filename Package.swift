720f9445104469ef01329b58e0eb1be4dad34abd
Alexander Chernousov <chrnsv@gmail.com>
2026-01-13 20:53:39 +0200

    Release 0.1

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "SURBinary",
-            url: "https://github.com/mugabe/SwiftUnusedResources/releases/download/0.0.10/sur-0.0.10.artifactbundle.zip",
-            checksum: "bd0b845e9c12e9a2e9fbdd3c686c8f1334013758d93182470c2d755e4d7d975d"
+            url: "https://github.com/mugabe/SwiftUnusedResources/releases/download/0.1.0/sur-0.1.0.artifactbundle.zip",
+            checksum: "b6155d99532a0ff1274828dd99622c8ae9db8ad4a5699a83348daf2c09b786ef"
         ),
     ]
 )
